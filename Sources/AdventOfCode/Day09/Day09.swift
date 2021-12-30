3446504188d9df5801e5b1ea2a21779eeae1d38d
Gereon Steffens <gereon@steffens.org>
2021-12-30 18:59:00 +0100

    day 9 part 2

--- Sources/AdventOfCode/Day09/Day09.swift
+++ Sources/AdventOfCode/Day09/Day09.swift
@@ -32,11 +32,32 @@ final class Day09: Day {
     private func part2(_ data: [String]) -> Int {
         let timer = Timer(day); defer { timer.show() }
 
-        return 0
+        return data.reduce(0) { sum, line in
+            sum + decodeV2(line).reduce(0) { sum, rle in
+                sum + rle.count()
+            }
+        }
     }
 
-    struct RLE {
-        let len, mul: Int
+    class RLE {
+        let length, multiplier: Int
+        var children: [RLE]?
+
+        init(length: Int, multiplier: Int) {
+            self.length = length
+            self.multiplier = multiplier
+        }
+
+        func count() -> Int {
+            if let children = children {
+                let sum = children.reduce(0) { sum, rle in
+                    sum + rle.count()
+                }
+                return sum * multiplier
+            } else {
+                return length * multiplier
+            }
+        }
     }
 
     func decode(_ str: String) -> String {
@@ -47,20 +68,20 @@ final class Day09: Day {
         var tmp = ""
         while let ch = it.next() {
             if ch == "(" && rle == nil {
-                var len = ""
-                var mul = ""
+                var len = 0
+                var mul = 0
                 while let ch = it.next(), ch != "x" {
-                    len.append(ch)
+                    len = len*10 + Int(String(ch))!
                 }
                 while let ch = it.next(), ch != ")" {
-                    mul.append(ch)
+                    mul = mul*10 + Int(String(ch))!
                 }
-                rle = RLE(len: Int(len)!, mul: Int(mul)!)
+                rle = RLE(length: len, multiplier: mul)
             }
             else if let r = rle {
                 tmp.append(ch)
-                if tmp.count == r.len {
-                    for _ in 0..<r.mul {
+                if tmp.count == r.length {
+                    for _ in 0..<r.multiplier {
                         result.append(tmp)
                     }
                     rle = nil
@@ -68,6 +89,38 @@ final class Day09: Day {
                 }
             } else {
                 result.append(ch)
+            }
+        }
+
+        return result
+    }
+
+    func decodeV2(_ str: String) -> [RLE] {
+        var it = str.makeIterator()
+        return decodeRLE(&it)
+    }
+
+    func decodeRLE(_ it: inout String.Iterator) -> [RLE] {
+        var result = [RLE]()
+        while let ch = it.next() {
+            if ch == "(" {
+                var len = 0
+                var mul = 0
+                while let l = it.next(), l != "x" {
+                    len = len*10 + Int(String(l))!
+                }
+                while let m = it.next(), m != ")" {
+                    mul = mul*10 + Int(String(m))!
+                }
+                let rle = RLE(length: len, multiplier: mul)
+                var str = ""
+                for _ in 0..<len {
+                    str.append(it.next()!)
+                }
+                rle.children = decodeV2(str)
+                result.append(rle)
+            } else {
+                result.append(RLE(length: 1, multiplier: 1))
             }
         }
 
