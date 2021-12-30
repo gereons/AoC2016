0b14d19047d1eced33eac29e6c3af469c863289c
Gereon Steffens <gereon@steffens.org>
2021-12-30 13:46:39 +0100

    day 6

--- Sources/AdventOfCode/Day06/Day06.swift
+++ Sources/AdventOfCode/Day06/Day06.swift
@@ -5,26 +5,67 @@
 //
 
 final class Day06: Day {
-    let testData = [ "", "", "" ]
+    let testData = [
+        "eedadn",
+        "drvtee",
+        "eandsr",
+        "raavrd",
+        "atevrs",
+        "tsrnev",
+        "sdttsa",
+        "rasrtv",
+        "nssdts",
+        "ntnada",
+        "svetve",
+        "tesnvt",
+        "vntsnd",
+        "vrdear",
+        "dvrsen",
+        "enarar"
+    ]
+
+    var chars = [[Character]]()
 
     func run() {
         // let data = testData
+        let data = Self.data.components(separatedBy: "\n")
 
         Timer.time(day) {
-            // parse data here
+            chars = data.map { line in
+                line.map { $0 }
+            }
         }
 
         print("Solution for part 1: \(part1())")
         print("Solution for part 2: \(part2())")
     }
 
-    private func part1() -> Int {
+    private func part1() -> String {
         let timer = Timer(day); defer { timer.show() }
-        return 0
+
+        var decrypted = ""
+        for i in 0 ..< chars[0].count {
+            var charCount = [Character: Int]()
+            for j in 0 ..< chars.count {
+                charCount[chars[j][i], default: 0] += 1
+            }
+            let ch = charCount.sorted { $0.value > $1.value }.first?.key
+            decrypted.append(ch!)
+        }
+        return decrypted
     }
 
-    private func part2() -> Int {
+    private func part2() -> String {
         let timer = Timer(day); defer { timer.show() }
-        return 0
+        var decrypted = ""
+        for i in 0 ..< chars[0].count {
+            var charCount = [Character: Int]()
+            for j in 0 ..< chars.count {
+                charCount[chars[j][i], default: 0] += 1
+            }
+            let ch = charCount.sorted { $0.value < $1.value }.first?.key
+            decrypted.append(ch!)
+        }
+        return decrypted
     }
 }
