b82ecc700b054de6163637277e10feaa18b5d454
Gereon Steffens <gereon@steffens.org>
2022-01-02 12:18:04 +0100

    day 15

--- Sources/AdventOfCode/Day15/Day15.swift
+++ Sources/AdventOfCode/Day15/Day15.swift
@@ -5,26 +5,64 @@
 //
 
 final class Day15: Day {
-    let testData = [ "", "", "" ]
+    struct Disc {
+        let size: Int
+        let start: Int
+
+        init(_ str: String) {
+            let tokens = str.split(separator: " ")
+            size = Int(tokens[3])!
+            start = Int(tokens[11].dropLast())!
+        }
+    }
+
+    let testData = [
+        "Disc #1 has 5 positions; at time=0, it is at position 4.",
+        "Disc #2 has 2 positions; at time=0, it is at position 1."
+    ]
+
+    var discs = [Disc]()
 
     func run() {
         // let data = testData
+        let data = Self.data.components(separatedBy: "\n")
 
         Timer.time(day) {
-            // parse data here
+            discs = data.map { Disc($0) }
         }
 
         print("Solution for part 1: \(part1())")
-        print("Solution for part 2: \(part2())")
+
+        discs.append(Disc("Disc #X has 11 positions; at time=0, it is at position 0."))
+        print("Solution for part 2: \(part1())")
     }
 
     private func part1() -> Int {
         let timer = Timer(day); defer { timer.show() }
-        return 0
+
+        return findTime()
     }
 
     private func part2() -> Int {
         let timer = Timer(day); defer { timer.show() }
-        return 0
+
+        return findTime()
+    }
+
+    private func findTime() -> Int {
+        for time in 0...Int.max {
+            var bounced = false
+            for (index, disc) in discs.enumerated() {
+                if (disc.start + time + index + 1) % disc.size != 0 {
+                    bounced = true
+                    break
+                }
+            }
+            if !bounced {
+                return time
+            }
+        }
+
+        fatalError()
     }
 }
