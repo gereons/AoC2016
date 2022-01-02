f52da1c8d38c8acb7ad824d9e8cd0400d8c42559
Gereon Steffens <gereon@steffens.org>
2022-01-02 13:10:52 +0100

    day 16

--- Sources/AdventOfCode/Day16/Day16.swift
+++ Sources/AdventOfCode/Day16/Day16.swift
@@ -5,26 +5,52 @@
 //
 
 final class Day16: Day {
-    let testData = [ "", "", "" ]
-
     func run() {
-        // let data = testData
-
-        Timer.time(day) {
-            // parse data here
-        }
-
         print("Solution for part 1: \(part1())")
         print("Solution for part 2: \(part2())")
     }
 
-    private func part1() -> Int {
+    private func part1() -> String {
         let timer = Timer(day); defer { timer.show() }
-        return 0
+
+        return patternChecksum(272)
     }
 
-    private func part2() -> Int {
+    private func part2() -> String {
         let timer = Timer(day); defer { timer.show() }
-        return 0
+
+        return patternChecksum(35651584)
+    }
+
+    private func patternChecksum(_ length: Int) -> String {
+        var input = Self.data.map { $0 == "1" }
+
+        while input.count < length {
+            input = grow(input)
+        }
+
+        let chksum = checksum(Array(input.prefix(length)))
+        return chksum.map { $0 ? "1" : "0" }.joined()
+    }
+
+    private func grow(_ input: [Bool]) -> [Bool] {
+        var b = [Bool]()
+        for ch in input.reversed() {
+            b.append(!ch)
+        }
+        return input + [false] + b
+    }
+
+    private func checksum(_ array: [Bool]) -> [Bool] {
+        var chk = [Bool](repeating: false, count: array.count / 2)
+        for (i0, i1) in stride(from: 0, through: array.count-1, by: 2).enumerated() {
+            chk[i0] = (array[i1] == array[i1+1])
+        }
+        assert(chk.count == array.count / 2)
+        if chk.count.isMultiple(of: 2) {
+            return checksum(chk)
+        } else {
+            return chk
+        }
     }
 }
